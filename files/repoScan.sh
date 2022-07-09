ae002d85690ba4ba7425f34567d29ac526cf8a82
ivan-tresoldi <67233434+ivan-tresoldi@users.noreply.github.com>
2022-07-09 09:43:26 +0200

    -

--- files/repoScan.sh
+++ files/repoScan.sh
@@ -1,3 +1,2 @@
 #!/bin/bash -l
-./files/twistcli -v
-#./twistcli coderepo scan --address https://$TL_CONSOLE --details -u $TL_USER -p $TL_PASS ./app
+./twistcli coderepo scan --address https://$TL_CONSOLE --details -u $TL_USER -p $TL_PASS ./app
