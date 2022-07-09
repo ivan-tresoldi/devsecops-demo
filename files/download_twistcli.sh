9f87ef077166bb0029384aa7c776e30e4625ecec
ivan-tresoldi <67233434+ivan-tresoldi@users.noreply.github.com>
2022-07-09 09:44:44 +0200

    -

--- files/download_twistcli.sh
+++ files/download_twistcli.sh
@@ -1,1 +1,1 @@
-curl -L -k -u $TL_USER:$TL_PASS https://$TL_CONSOLE/api/v1/util/twistcli > twistcli; chmod a+x twistcli;
+#curl -L -k -u $TL_USER:$TL_PASS https://$TL_CONSOLE/api/v1/util/twistcli > twistcli; chmod a+x twistcli;
