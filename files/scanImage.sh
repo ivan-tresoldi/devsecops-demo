4383415ecbb7725ad75b547951c6fe1c275b5b2e
ivan-tresoldi <67233434+ivan-tresoldi@users.noreply.github.com>
2022-07-21 16:29:44 +0200

    Update scanImage.sh

--- files/scanImage.sh
+++ files/scanImage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo docker pull itresoldi/evilpetclinic:latest     
-./twistcli images scan --u $TL_USER --p $TL_PASS --address https://$TL_CONSOLE --details itresoldi/evilpetclinic -o output.json
+sudo ./twistcli images scan --u $TL_USER --p $TL_PASS --address https://$TL_CONSOLE --details itresoldi/evilpetclinic -o output.json
